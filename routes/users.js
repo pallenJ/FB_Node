14a3cf6904d9f9d10d917367d3e58cfabe18012a
JoonMo <pallennellap17@gmail.com>
2021-05-31 17:26:50 +0900

    .

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var firebase = require('firebase')
+var firebaseui = require('firebaseui');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
