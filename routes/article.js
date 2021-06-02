8d31b48c3c92b026a05e5a3fccd44797c8108420
JoonMo <pallennellap17@gmail.com>
2021-06-02 11:38:11 +0900

    프로젝트 테스트

--- routes/article.js
+++ routes/article.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase').default;
+var functions = require('firebase-functions');
+var log = functions.logger;
+
 var config = {
     apiKey: "AIzaSyCEsUYmEz_qu9V8RSLjPKI3ZwRXn1lVKDQ",
     authDomain: "https://joonmohome.firebaseapp.com/",
@@ -10,19 +13,39 @@ var config = {
     messagingSenderId: "407521114451"
 };
 firebase.initializeApp(config)
-var db = firebase.firestore().collection('board');
+var db = firebase.firestore().collection('article');
 
-var log = require('log4js').getLogger();
 /* GET home page. */
-router.get('/', function(req, res, next) {});
+router.get('/', function(req, res, next) {
+    return null;
+});
 
 router.post('/save', async(req, res, next) => {
-    log.debug('asdfsdaf');
-    var { title, content } = req.body;
-    const boardData = { title: title, content: content };
-    await db.add(boardData);
+    var { bno, title, content } = req.body;
+    var boardData = { bno: "", title: title, content: content, status: "added", added: null, lastEdit: null };
+    if (bno) {
+        boardData.bno = bno;
+        boardData.status = "edited";
+        boardData.added = (await db.doc(bno).get()).data().added;
+        boardData.lastEdit = Date.now();
+        await db.doc(bno).update(boardData);
+    } else {
+        boardData.added = Date.now();
+        var doc = db.doc();
+        boardData.bno = doc.id;
+        await doc.set(boardData);
+    }
     res.json(boardData);
 });
 
+
+
+
+router.delete('/remove', async(req, res, next) => {
+    if (!req.query.bno) res.json("bno is null");
+    var deletedData;
+    await (deletedData = db.doc(req.query.bno)).delete();
+    res.json(deletedData);
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var functions = require('firebase-functions');
+var log = functions.logger;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    log.debug('log test');
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
