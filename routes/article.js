dbf9c02f810dcf0ea105c4e48265e07134649ce4
JoonMo <pallennellap17@gmail.com>
2021-06-05 16:58:20 +0900

    페이징 수정

--- routes/article.js
+++ routes/article.js
@@ -22,8 +22,6 @@ router.get('/', function(req, res, next) {
     return null;
 });
 
-
-
 router.get('/list', async(req, res, next) => {
     var pg = req.query.pg || 2;
     var rows = req.query.rows || 3;
@@ -31,17 +29,18 @@ router.get('/list', async(req, res, next) => {
     var articleList = [];
     var articleTemp;
     try {
-        var articleDB = db.orderBy('added', 'desc'); //작성일자기준 최신~오래된순 정렬
+        let articleDB = db.orderBy('added', 'desc'); //작성일자기준 최신~오래된순 정렬
         maxLength = (await articleDB.get()).size;
-        if ((pg - 1) * rows > maxLength) {
+
+        if ((pg - 1) * rows > maxLength || maxLength == 0) {
             res.json('page end'); //전체 페이지보다 현재 선택한 페이지*행의 개수가 많을때
         }
-        var startData = (await articleDB.limitToLast(Math.max(pg - 1 * rows, 1)).get()).docs[0];
-        if (pg * rows - 1 > maxLength) {
-            rows = pg * rows - maxLength - 1; //선택한 페이지가 마지막 페이지일때
+        let startData = (await articleDB.limit(Math.max((pg - 1) * rows, 1)).get()).docs.reverse()[0];
+        if (pg * rows > maxLength && (rows = maxLength - (pg - 1) * rows) == 0) {
+            res.json('page end'); //전체 페이지보다 현재 선택한 페이지*행의 개수가 많을때
         }
+        let pageList = await (pg == 1 ? articleDB.startAt(startData) : articleDB.startAfter(startData)).limit(rows).get();;
 
-        var pageList = await articleDB.startAfter(startData.id).limit(rows).get();
         pageList.forEach(_data => {
             articleTemp = _data.data();
             articleTemp.added = dateFormat(articleTemp.added, 'yyyy-mm-dd HH:MM:ss');
@@ -53,43 +52,60 @@ router.get('/list', async(req, res, next) => {
         res.json(error);
     }
 
-    // db.orderBy('added', 'desc').get().then((ss) => {
-    //     let rs = [];
-    //     ss.docs.forEach(
-    //         _e => {
-
-    //             log.log(_e.data(), "/", typeof(_e));
-    //             rs.push(_e.data())
-    //         }
-    //     );
-    //     res.json(rs);
-    // }).catch(err => res.render(err));
-
 });
 
-router.post('/save', async(req, res, next) => {
-    let articleTemp = Article(req.body);
-    let status = 'added';
-    if (articleTemp.bno) {
-        articleTemp.added = (await db.doc(articleTemp.bno).get()).data().added;
-        articleTemp.edited = Date.now();
-        await db.doc(articleTemp.bno).update(articleTemp);
-        status = 'edited';
-    } else {
-        let doc = db.doc();
-        articleTemp.bno = doc.id;
-        await doc.set(articleTemp);
-    }
 
-    res.json({ article: articleTemp, status: status });
+router.get('/:bno', async(req, res, next) => {
+    try {
+        let articleData = (await db.doc(req.params.bno).get()).data();
+        articleData.added = dateFormat(articleData.added, 'yyyy-mm-dd HH:MM:ss');
+        articleData.edited = dateFormat(articleData.edited, 'yyyy-mm-dd HH:MM:ss');
+        res.json(articleData)
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+
+
+router.post('/save', async(req, res, next) => {
+    try {
+        let articleTemp = Article(req.body); // request에서 값을 가져옴
+        let status = 'added';
+        if (articleTemp.bno) { //보낸값에 bno가 있는 경우(이미 존재하는 글을 수정할 경우)
+            let edittemp;
+            if ((edittemp = await db.where('bno', '==', articleTemp.bno).get()).size == 0) //글이  존재하는지체크
+                res.json({ error: 'article is not exist' })
+            edittemp = edittemp.docs[0];
+            articleTemp.added = edittemp.data().added;
+            articleTemp.edited = Date.now();
+            await db.doc(articleTemp.bno).update(articleTemp);
+            status = 'edited';
+        } else { //새로운 글을 추가할경우
+            let doc = db.doc();
+            articleTemp.bno = doc.id;
+            await doc.set(articleTemp);
+        }
+        res.json({ article: articleTemp, status: status });
+
+    } catch (error) {
+        res.json(error);
+    }
 });
 
 
 router.delete('/remove', async(req, res, next) => {
-    if (!req.query.bno) res.json("bno is null");
-    let deletedData;
-    await (deletedData = db.doc(req.query.bno)).delete();
-    res.json(deletedData);
+    try {
+
+        if (!req.query.bno) res.json("bno is null");
+        let deletedData;
+        await (deletedData = db.doc(req.query.bno)).delete();
+        res.json(deletedData);
+    } catch (error) {
+        res.json(error);
+    }
 })
+
+
 
 module.exports = router;
